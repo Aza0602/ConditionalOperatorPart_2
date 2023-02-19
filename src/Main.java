446348cae849dfc58d898e51e29d194e410e8b62
Azat <aelchikushuulu@gmail.com>
2023-02-19 17:42:29 +0600

    Task 5

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
 
     public static void task1 () {
@@ -77,5 +78,37 @@ public class Main {
             System.out.println("Свыше 100 км доставки нет.");
         }
 
+    }
+
+    public static void task5 () {
+
+        System.out.println("Задача 5");
+
+        int monthNumber = 12;
+
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("null");
+        }
     }
 }
