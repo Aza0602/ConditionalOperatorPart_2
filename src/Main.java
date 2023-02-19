b47606b84ef7003803b73d590017ecb9d4bc83b9
Azat <aelchikushuulu@gmail.com>
2023-02-19 17:36:58 +0600

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1 () {
@@ -16,6 +17,28 @@ public class Main {
         if (clientOS == 0 && clientOS != 1) {
             System.out.println("«Установите версию приложения для iOS по ссылке». ");
         } else {
+            System.out.println("«Установите версию приложения для Android по ссылке».");
+        }
+    }
+
+    public static void task2 () {
+
+        System.out.println("Задача 2");
+
+        Scanner os = new Scanner(System.in);
+        System.out.println("Введите число 0 если у вас IOS либо число 1 если у вас Android.");
+        int clientOS = os.nextInt();
+        Scanner deviceYear = new Scanner(System.in);
+        System.out.println("Введите год выпуска вашего устройства.");
+        int clientDeviceYear = deviceYear.nextInt();
+
+        if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("«Установите облегченную версию приложения для iOS по ссылке».");
+        } else if (clientDeviceYear < 2015 && clientOS == 1){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке».");
+        } else if (clientDeviceYear >= 2015 && clientOS == 0) {
+            System.out.println("«Установите версию приложения для iOS по ссылке». ");
+        } else if (clientDeviceYear >= 2015 && clientOS == 1) {
             System.out.println("«Установите версию приложения для Android по ссылке».");
         }
     }
